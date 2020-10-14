c681a39ea048bcfbf3a8fcaa590c4f6d36e0db16
jh3y <jh3y@users.noreply.github.com>
2020-10-14 18:41:34 +0100

    feat: try broadcaster with node exec

--- broadcaster.js
+++ broadcaster.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const { DateTime } = require('luxon')
-const { cd, echo, exec, rm, which } = require('shelljs')
+const { cd, echo, rm, which } = require('shelljs')
+const { exec }  = require('child_process')
 
 const GITHUB_USERNAME = 'jh3y'
 const GITHUB_REPOSITORY = 'vincent-van-git-test'
@@ -11,6 +12,7 @@ const REPO_LOCATION = `${process.cwd()}/${REPO_DIR}`
 const broadcast = async arr => {
   const TODAY = DateTime.local()
   const START_DAY = TODAY.minus({days: arr.length - 1})
+  echo(TODAY)
   // console.info(arr)
   // console.info(TODAY)
   // console.info(START_DAY)
@@ -29,33 +31,33 @@ const broadcast = async arr => {
    */
 
   // 3. Create a repository in a directory.
-  // if (which('git')) {
-  //   // Remove the repo directory if it exists
-  //   try {
-  //     await fs.promises.access(REPO_LOCATION)
-  //     rm('-rf', REPO_LOCATION)
-  //   } catch (err) {
-  //     console.info('VVG: No repo directory exists.')
-  //   }
-  //   // Create the repo directory
-  //   await fs.promises.mkdir(REPO_LOCATION)
-  //   // Initialise the git repo
-  //   cd(REPO_LOCATION)
-  //   exec('git init')
-  //   exec('say new git repo initialized')
-  //   // Loop through the array matching up the dates and creating empty commits
-  //   for (let d = 0; d < arr.length; d++) {
-  //     // git commit --allow-empty --date "Mon Oct 12 23:17:02 2020 +0100" -m "Vincent paints again"
-  //     const LEVEL = arr[d]
-  //     const NUMBER_COMMITS = LEVEL * 50
-  //     if (NUMBER_COMMITS > 0) {
-  //       const COMMIT_DAY = START_DAY.plus({ days: d })
-  //       for (let c = 0; c < NUMBER_COMMITS; c++) {
-  //         exec(`git commit --allow-empty --date "${COMMIT_DAY.toHTTP()}" -m "Vincent paints again"`)
-  //       }
-  //     }
-  //   }
-  // }
+  if (which('git')) {
+    // Remove the repo directory if it exists
+    try {
+      await fs.promises.access(REPO_LOCATION)
+      rm('-rf', REPO_LOCATION)
+    } catch (err) {
+      console.info('VVG: No repo directory exists.')
+    }
+    // Create the repo directory
+    await fs.promises.mkdir(REPO_LOCATION)
+    // Initialise the git repo
+    cd(REPO_LOCATION)
+    exec('git init')
+    exec('say new git repo initialized')
+    // Loop through the array matching up the dates and creating empty commits
+    for (let d = 0; d < arr.length; d++) {
+      // git commit --allow-empty --date "Mon Oct 12 23:17:02 2020 +0100" -m "Vincent paints again"
+      const LEVEL = arr[d]
+      const NUMBER_COMMITS = LEVEL * 50
+      if (NUMBER_COMMITS > 0) {
+        const COMMIT_DAY = START_DAY.plus({ days: d })
+        for (let c = 0; c < NUMBER_COMMITS; c++) {
+          exec(`git commit --allow-empty --date "${COMMIT_DAY.toHTTP()}" -m "Vincent paints again"`)
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
