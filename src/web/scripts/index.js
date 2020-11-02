42a86cd0688a02eeec09d59ff1c61ba576464fb9
jh3y <jh3y@users.noreply.github.com>
2020-11-02 21:19:31 +0000

    feat: integrate grid wipe

--- src/web/scripts/index.js
+++ src/web/scripts/index.js
@@ -5,6 +5,13 @@ import InfoDrawer from '../../shared/components/info-drawer'
 import SettingsDrawer from '../../shared/components/settings-drawer'
 import Actions from '../../shared/components/actions'
 import Progress from '../../shared/components/progress'
+import Toasts from '../../shared/components/toasts'
+import useSound from '../../shared/hooks/useSound'
+import { MESSAGES } from '../../shared/constants'
+import CLICK_PATH from '../../shared/assets/audio/click.mp3'
+import SPARKLE_PATH from '../../shared/assets/audio/sparkle.mp3'
+import TRUMPET_PATH from '../../shared/assets/audio/trumpet-fanfare.mp3'
+import BRUSH_PATH from '../../shared/assets/audio/brush-stroke.mp3'
 
 import 'regenerator-runtime/runtime'
 import '../styles/index.styl'
@@ -15,25 +22,56 @@ const ROOT_NODE = document.querySelector('#app')
 const URL = '/.netlify/functions/vincent'
 const App = () => {
   const [coding, setCoding] = useState(false)
+  const [cleared, setCleared] = useState(null)
+  const [toast, setToast] = useState(null)
+  const [muted, setMuted] = useState(false)
   const [generating, setGenerating] = useState(false)
+  const [image, setImage] = useState(false)
+  const [dirty, setDirty] = useState(false)
+  const [imageName, setImageName] = useState(null)
+  const { play: clickPlay } = useSound(CLICK_PATH)
+  const { play: sparklePlay } = useSound(SPARKLE_PATH)
+  const { play: brushPlay } = useSound(BRUSH_PATH)
+  const { play: trumpetPlay } = useSound(TRUMPET_PATH)
+
   const NUMBER_OF_DAYS = 52 * 7 + (new Date().getDay() + 1)
   const cellsRef = useRef(new Array(NUMBER_OF_DAYS).fill(0))
 
-  const generateScript = async () => {
+  const onGenerate = async () => {
     console.info('cool')
     const resp = await (await fetch(URL)).json()
     console.info(resp)
+  }
+
+  const checkDirty = () => {
+    setDirty(cellsRef.current.filter((cell) => cell !== 0).length > 0)
+  }
+
+  const onWipe = () => {
+    if (!muted) clickPlay()
+    if (confirm(MESSAGES.CONFIRM_WIPE)) {
+      cellsRef.current = new Array(NUMBER_OF_DAYS).fill(0)
+      setImage('') // Setting to empty string to select default.
+      setImageName('')
+      setCleared(new Date().toUTCString())
+      setDirty(false)
+      if (!muted) brushPlay()
+      // ipcRenderer.send(MESSAGING_CONSTANTS.INFO, {
+      //   message: MESSAGES.WIPED,
+      // })
+    }
   }
 
   return (
     <Fragment>
       <SettingsDrawer />
       <InfoDrawer />
-      <div className='canvas'>
-        <CommitGrid cells={cellsRef.current} />
-        <Actions />
+      <div className="canvas">
+        <CommitGrid key={cleared} cells={cellsRef.current} onChange={checkDirty} />
+        <Actions disabled={!dirty} onGenerate={onGenerate} onWipe={onWipe} />
       </div>
       {(coding || generating) && <Progress />}
+      <Toasts toast={toast} />
     </Fragment>
   )
 }
