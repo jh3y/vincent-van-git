9f846ae2d634c72e28b70b0ea7c48a54a1a4be61
jh3y <jh3y@users.noreply.github.com>
2022-03-16 18:31:57 +0000

    fix: resolve #14 by adding logo and Github corner to app

--- src/web/scripts/index.js
+++ src/web/scripts/index.js
@@ -30,6 +30,7 @@ import CLICK_PATH from '../../shared/assets/audio/click.mp3'
 import SPARKLE_PATH from '../../shared/assets/audio/sparkle.mp3'
 import TRUMPET_PATH from '../../shared/assets/audio/trumpet-fanfare.mp3'
 import BRUSH_PATH from '../../shared/assets/audio/brush-stroke.mp3'
+import Logo from '../../shared/assets/icons/logo64.png'
 
 import 'regenerator-runtime/runtime'
 import '../../shared/styles/shared.styl'
@@ -404,6 +405,26 @@ const App = () => {
           cellsRef={cellsRef}
         />
       </div>
+      <a
+        className="github-corner"
+        href="https://github.com/jh3y/vincent-van-git"
+        title="Fork me on GitHub"
+        target="_blank"
+        rel="noopener noreferrer">
+        <svg width="80" height="80" viewBox="0 0 250 250">
+          <title>Fork me on GitHub</title>
+          <path d="M0 0h250v250"></path>
+          <path
+            className="octo-arm"
+            d="M127.4 110c-14.6-9.2-9.4-19.5-9.4-19.5 3-7 1.5-11 1.5-11-1-6.2 3-2 3-2 4 4.7 2 11 2 11-2.2 10.4 5 14.8 9 16.2"
+            fill="currentColor"></path>
+          <path
+            className="octo-body"
+            d="M113.2 114.3s3.6 1.6 4.7.6l15-13.7c3-2.4 6-3 8.2-2.7-8-11.2-14-25 3-41 4.7-4.4 10.6-6.4 16.2-6.4.6-1.6 3.6-7.3 11.8-10.7 0 0 4.5 2.7 6.8 16.5 4.3 2.7 8.3 6 12 9.8 3.3 3.5 6.7 8 8.6 12.3 14 3 16.8 8 16.8 8-3.4 8-9.4 11-11.4 11 0 5.8-2.3 11-7.5 15.5-16.4 16-30 9-40 .2 0 3-1 7-5.2 11l-13.3 11c-1 1 .5 5.3.8 5z"
+            fill="currentColor"></path>
+        </svg>
+      </a>
+      <img className="branding" src={Logo} alt="Vincent van Git" />
       {generating && <Progress hide={hideVincent} onComplete={onProgressEnd} />}
       <AudioToggle onToggle={toggleAudio} muted={muted} />
       <Toasts toast={toast} />
